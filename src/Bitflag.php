1ae553229f2c626a46eb29a4f4dbad97af265964
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-23 09:46:45 +0200

    bitflag callback handler

---
+++ src/Bitflag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ornament;
+
+trait Bitflag
+{
+    use Callback;
+
+    public function addBitflag($name, $bit, $source)
+    {
+        $name = ucfirst(Helper::denormalize($name));
+        $this->callback("get$name", function () use ($bit, $source) {
+            return $this->$source & $bit;
+        });
+        $this->callback("set$name", function ($val) use ($bit, $source) {
+            if ($val) {
+                $this->$source |= $bit;
+            } else {
+                $this->$source &= ~$bit;
+            }
+        });
+    }
+}
+
