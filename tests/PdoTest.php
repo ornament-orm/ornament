51e67089942e70eed323248723ad3800e8b5d521
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-25 10:31:43 +0200

    fix tests, use dependency injection

--- tests/PdoTest.php
+++ tests/PdoTest.php
@@ -1,30 +1,46 @@
 <?php
 
+use Disclosure\Container;
+use Disclosure\Injector;
+
 class MyTableModel
 {
     use Ornament\Pdo;
+    use Injector;
+
+    private $pdo;
 
     public $id;
     public $name;
     public $comment;
 
-    public function __construct(PDO $pdo)
+    public function __construct()
     {
-        $this->addPdoAdapter($pdo);
+        $this->inject(function ($pdo) {});
+        $this->addPdoAdapter($this->pdo);
     }
 }
 
 class LinkedTableModel
 {
     use Ornament\Pdo;
+    use Ornament\Autoload;
+    use Injector;
+
+    private $pdo;
 
     public $id;
+    /**
+     * @Model MyTableModel
+     * @Constructor [ pdo ]
+     */
     public $mytable;
     public $points;
 
-    public function __construct(PDO $pdo)
+    public function __construct()
     {
-        $this->addPdoAdapter($pdo);
+        $this->inject(function ($pdo) {});
+        $this->addPdoAdapter($this->pdo);
     }
 
     public function getPercentage()
@@ -41,16 +57,20 @@ class BitflagModel
 {
     use Ornament\Pdo;
     use Ornament\Bitflag;
+    use Injector;
 
     const STATUS_NICE = 1;
     const STATUS_CATS = 2;
     const STATUS_CODE = 4;
 
+    private $pdo;
+
     public $status;
 
-    public function __construct(PDO $pdo)
+    public function __construct()
     {
-        $this->addPdoAdapter($pdo);
+        $this->inject(function ($pdo) {});
+        $this->addPdoAdapter($this->pdo);
         $this->addBitflag('nice', self::STATUS_NICE, 'status');
         $this->addBitflag('cats', self::STATUS_CATS, 'status');
         $this->addBitflag('code', self::STATUS_CODE, 'status');
@@ -74,12 +94,16 @@ class PdoTest extends PHPUnit_Extensions_Database_TestCase
         $stmt = self::$pdo->prepare("SELECT * FROM my_table");
         $stmt->execute();
         $rows = $stmt->fetchAll();
-        $this->assertEquals(1, count($rows));
+        $this->assertEquals(4, count($rows));
         $model->comment = 'Awesome';
         $model->save();
         $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $this->assertEquals('Awesome', $row['comment']);
+        $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->assertEquals('Awesome', $row[3]['comment']);
+        unset($model);
+        $model = new MyTableModel(self::$pdo);
+        $model->id = 1;
+        $model->load();
     }
 
     /**
@@ -110,12 +134,40 @@ class PdoTest extends PHPUnit_Extensions_Database_TestCase
         $this->assertEquals(3, $model->status);
     }
 
+    public function testAutoload()
+    {
+        $model = new MyTableModel(self::$pdo);
+        $model->name = 'Marijn';
+        $model->comment = 'Hi Ornament';
+        $model->save();
+        $linked = new LinkedTableModel(self::$pdo);
+        $linked->mytable = $model->id;
+        $linked->points = 4;
+        $linked->save();
+        unset($model, $linked);
+        $linked = new LinkedTableModel(self::$pdo);
+        $linked->id = 1;
+        $linked->load();
+        $this->assertEquals('MyTableModel', get_class($linked->mytable));
+    }
+
+    public function testQuery()
+    {
+        $model = new MyTableModel(self::$pdo);
+        $list = $model->query([]);
+        $this->assertEquals(3, count($list));
+        $this->assertEquals('MyTableModel', get_class($list[0]));
+    }
+
     public function getConnection()
     {
         if ($this->conn === null) {
             if (!isset(self::$pdo)) {
                 self::$pdo = new PDO('sqlite::memory:');
                 self::$pdo->exec(file_get_contents(__DIR__.'/schema.sql'));
+                Container::inject('*', function (&$pdo) {
+                    $pdo = self::$pdo;
+                });
             }
             $this->conn = $this->createDefaultDBConnection(self::$pdo, 'test');
         }
