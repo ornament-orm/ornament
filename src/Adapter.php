4ea8b50f28b48a5e693e4c3c5e694f715f95cf49
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-25 10:21:49 +0200

    extend interface

--- src/Adapter.php
+++ src/Adapter.php
@@ -4,6 +4,8 @@ namespace Ornament;
 
 interface Adapter
 {
+    public function query(Model $model, array $parameters, array $ctor = []);
+    public function load(Model $model);
     public function create(Model $model);
     public function update(Model $model);
     public function delete(Model $model);
