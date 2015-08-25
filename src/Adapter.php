2936a4a693ee95b6dfc419bca247efbf27133b88
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-25 10:30:00 +0200

    better declaration, apparently we cannot typehint for random objects like in javascript

--- src/Adapter.php
+++ src/Adapter.php
@@ -4,7 +4,7 @@ namespace Ornament;
 
 interface Adapter
 {
-    public function query(Model $model, array $parameters, array $ctor = []);
+    public function query($model, array $parameters, array $ctor = []);
     public function load(Model $model);
     public function create(Model $model);
     public function update(Model $model);
