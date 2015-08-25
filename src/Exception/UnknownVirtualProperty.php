3fc7165ab016ab19de4780d15f930ad6831697b8
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-25 16:19:33 +0200

    add actual implementation

--- src/Exception/UnknownVirtualProperty.php
+++ src/Exception/UnknownVirtualProperty.php
@@ -4,5 +4,14 @@ namespace Ornament\Exception;
 
 class UnknownVirtualProperty extends \Exception
 {
+    public function __construct($prop, $object, Exception $prev = null)
+    {
+        $message = sprintf(
+            'Unknown virtual property %s on %s',
+            $prop,
+            get_class($object)
+        );
+        parent::__construct($message, 0, $prev);
+    }
 }
 
