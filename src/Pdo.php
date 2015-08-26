8ce0a88a1f2df82d7e4e99461877666420dac7d3
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-26 07:49:34 +0200

    allow defining primary keys by annotation

--- src/Pdo.php
+++ src/Pdo.php
@@ -25,13 +25,18 @@ trait Pdo
                 }
             }
         }
-        $pk = false;
-        if (in_array('id', $fields)) {
-            $pk = true;
+        $pk = [];
+        foreach ($annotations as $prop => $anno) {
+            if (isset($anno['PrimaryKey'])) {
+                $pk[] = $prop;
+            }
+        }
+        if (!$pk && in_array('id', $fields)) {
+            $pk[] = 'id';
         }
         $adapter = new Adapter\Pdo($pdo, $id, $fields);
         if ($pk) {
-            $adapter->setPrimaryKey('id');
+            call_user_func_array([$adapter, 'setPrimaryKey'], $pk);
         }
         return $this->addAdapter($adapter, $id, $fields);
     }
