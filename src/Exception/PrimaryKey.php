c1914812efa9e4beb3ef955194488ecdd9e79ba0
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-26 07:40:54 +0200

    add proper constructor

--- src/Exception/PrimaryKey.php
+++ src/Exception/PrimaryKey.php
@@ -2,7 +2,18 @@
 
 namespace Ornament\Exception;
 
-class PrimaryKey extends \Exception
+use Exception as Ex;
+
+class PrimaryKey extends Ex
 {
+    public function __construct($model, $field, $code = 0, Ex $prev = null)
+    {
+        $message = sprintf(
+            'Missing primary key %s on model %s',
+            $field,
+            get_class($model)
+        );
+        parent::__construct($message, $code, $prev);
+    }
 }
 
