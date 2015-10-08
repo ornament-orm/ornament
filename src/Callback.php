fe34e3c96eae034fa72e56a46ed763243406d134
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-08 09:07:02 +0200

    these should also be exposed to we can export virtuals defined by callbacks

--- src/Callback.php
+++ src/Callback.php
@@ -4,26 +4,36 @@ namespace Ornament;
 
 trait Callback
 {
+    private static $callbacks = [];
+
     private function callback($fn, $args = null)
     {
-        static $callbacks = [];
         if (is_callable($args)) {
-            $callbacks[$fn] = $args;
+            self::$callbacks[$fn] = $args;
             return;
         }
-        if (!isset($callbacks[$fn])) {
+        if (!isset(self::$callbacks[$fn])) {
             throw new Exception\UndefinedCallback;
         }
         if (!isset($args)) {
             // Just checking...
             return true;
         }
-        return call_user_func_array($callbacks[$fn], $args);
+        return call_user_func_array(self::$callbacks[$fn], $args);
     }
 
     public function __call($fn, array $args = [])
     {
         return $this->callback($fn, $args);
+    }
+
+    private function listVirtualCallbackProperties()
+    {
+        $props = [];
+        foreach (self::$callbacks as $name => $value) {
+            $props[] = Helper::normalize(substr($name, 3));
+        }
+        return $props;
     }
 }
 
