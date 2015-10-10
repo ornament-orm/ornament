21bf037236a3d263f5779dcf4fb4f3ffd353c027
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-10 10:44:17 +0200

    implement update in a sane way

--- src/Observer.php
+++ src/Observer.php
@@ -3,9 +3,28 @@
 namespace Ornament;
 
 use SplSubject;
+use ReflectionClass;
+use ReflectionMethod;
 
 trait Observer
 {
-    public abstract function update(SplSubject $subject);
+    public function update(SplSubject $subject)
+    {
+        static $reflection;
+        if (!isset($reflection)) {
+            $ref = new ReflectionClass($this);
+            foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+                $args = $method->getParameters();
+                if (count($args) != 1 || $method->getName() == 'update') {
+                    continue;
+                }
+                if ($hinted = $args[0]->getClass()
+                    and $subject instanceof $hinted->name
+                ) {
+                    call_user_func([$this, $method->getName()], $subject);
+                }
+            }
+        }
+    }
 }
 
