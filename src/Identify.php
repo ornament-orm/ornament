6ed8da842902cdc2659b4f48ae4014ece0bb74ff
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-10 09:16:02 +0200

    renamed

---
+++ src/Identify.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ornament;
+
+/**
+ * Helper trait to guesstimate the identifier (table name, API endpoint etc.)
+ * for a given model. Mostly useful for PDO-like adapters, but theoretically
+ * other adapters could implement similar logic.
+ */
+trait Identify
+{
+    /**
+     * Return the guesstimated identifier, optionally by using the callback
+     * passed as an argument.
+     *
+     * @param callable $fn Optional callback doing the guesstimating.
+     * @return string A guesstimated identifier.
+     */
+    public function guessIdentifier(callable $fn = null)
+    {
+        static $guesser;
+        static $table;
+        if (isset($table)) {
+            return $table;
+        }
+        if (isset($fn)) {
+            $guesser = $fn;
+        }
+        if (!isset($guesser)) {
+            $guesser = function ($class) {
+                $class = preg_replace('@\\\\?Model$@', '', $class);
+                return Helper::normalize($class);
+            };
+        }
+        return $guesser(get_class($this));
+    }
+}
+
