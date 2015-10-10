9379c676da014076d4411a32c6fbd5ef983f0da2
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-10 11:27:51 +0200

    don't cache, but mostly look for annotations too

--- src/Observer.php
+++ src/Observer.php
@@ -5,24 +5,38 @@ namespace Ornament;
 use SplSubject;
 use ReflectionClass;
 use ReflectionMethod;
+use zpt\anno\Annotations;
 
 trait Observer
 {
     public function update(SplSubject $subject)
     {
-        static $reflection;
-        if (!isset($reflection)) {
-            $ref = new ReflectionClass($this);
-            foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-                $args = $method->getParameters();
-                if (count($args) != 1 || $method->getName() == 'update') {
+        $ref = new ReflectionClass($this);
+        foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            $args = $method->getParameters();
+            if (count($args) != 1 || $method->getName() == 'update') {
+                continue;
+            }
+            if ($hinted = $args[0]->getClass()
+                and $subject instanceof $hinted->name
+            ) {
+                $fn = $method->getName();
+                $annotations = new Annotations($method);
+                if (isset($annotations['Blind'])) {
                     continue;
                 }
-                if ($hinted = $args[0]->getClass()
-                    and $subject instanceof $hinted->name
-                ) {
-                    call_user_func([$this, $method->getName()], $subject);
+                if (isset($annotations['NotifyForState'])) {
+                    $states = [];
+                    if (is_array($annotations['NotifyForState'])) {
+                        $states = $annotations['NotifyForState'];
+                    } else {
+                        $states[] = $annotations['NotifyForState'];
+                    }
+                    if (!in_array($subject->state(), $states)) {
+                        continue;
+                    }
                 }
+                $this->$fn($subject);
             }
         }
     }
