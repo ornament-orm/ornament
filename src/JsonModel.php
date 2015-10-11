1a442558fec2314202c7537715645041c7856f09
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-11 09:42:32 +0200

    jsonmodel trait

---
+++ src/JsonModel.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Ornament;
+
+use JsonSerializable;
+use ReflectionClass;
+
+trait JsonModel
+{
+    /**
+     * Returns a hash of properties/values suitable for Json serialization.
+     *
+     * @return array Json serializable array representation of this model.
+     */
+    public function jsonSerialize()
+    {
+        static $reflected, $callbacks;
+        if (!isset($reflected, $callbacks)) {
+            $annotations = $this->annotations();
+            $refclass = new ReflectionClass($this);
+            $reflected = [];
+            $callbacks = [];
+            foreach ($refclass->getProperties() as $prop) {
+                if ($prop->isStatic()) {
+                    continue;
+                }
+                if (!$prop->isPublic()) {
+                    if (isset(
+                        $annotations['properties'][$prop->getName()],
+                        $annotations['properties'][$prop->getName()]['Private']
+                    )) {
+                        continue;
+                    }
+                }
+                $reflected[] = $prop->getName();
+            }
+            foreach ($refclass->getMethods() as $method) {
+                if (preg_match('@^get[A-Z]@', $method->getName())) {
+                    $name = Helper::normalize(preg_replace(
+                        '@^get@',
+                        '',
+                        $method->getName()
+                    ));
+                    $callbacks[$name] = $method->getName();
+                }
+            }
+        }
+        $json = [];
+        foreach ($reflected as $prop) {
+            $json[$prop] = $this->$prop;
+        }
+        foreach ($callbacks as $name => $callback) {
+            $json[$name] = call_user_func([$this, $callback]);
+        }
+        foreach ($json as &$value) {
+            if (is_object($value) && $value instanceof JsonSerializable) {
+                $value = (object)$value->jsonSerialize();
+            }
+        }
+        return $json;
+    }
+}
+
