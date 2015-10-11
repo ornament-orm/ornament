5ab2ea1c3ec39967171fe05ab32cdab2b609ddfa
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-11 14:36:13 +0200

    these should be part of the interface

--- src/Adapter.php
+++ src/Adapter.php
@@ -4,6 +4,9 @@ namespace Ornament;
 
 interface Adapter
 {
+    public function setPrimaryKey($field);
+    public function setIdentifier($identifier);
+    public function setFields(array $fields);
     public function query($model, array $ps, array $opts = []);
     public function load(Container $model);
     public function create(Container $model);
