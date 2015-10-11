c6e5fb0cf2707c9962fae185ceaf435cc090ab90
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-11 09:39:09 +0200

    turn this into a _class_, using traits is actually stupid here

--- src/Bitflag.php
+++ src/Bitflag.php
@@ -2,44 +2,61 @@
 
 namespace Ornament;
 
+use JsonSerializable;
+
 /**
- * Trait to quickly add bitflags to a model.
+ * Object to emulate a bitflag in Ornament models.
  *
  * For a model Foo with a property 'status', we often want to define a number of
  * bitflags, e.g. 'status_on = 1', 'status_valid = 2' etc. The Bitflag trait
- * makes this easy:
+ * makes this easy.
+ *
+ * Annotate the bitflag property with @Bitflag followed by a map of names/flags,
+ * e.g. @Bitglag on = 1, valid = 2
  *
  * <code>
- * $this->addBitflag('on', 1, 'status');
  * // Now this works, assuming `$model` is the instance:
- * var_dump($model->on); // true or false, depending
- * $model->on = true; // bit 1 is now on (status |= 1)
- * $model->on = false; // bit 1 is now off (status &= ~1)
+ * var_dump($model->status->on); // true or false, depending
+ * $model->status->on = true; // bit 1 is now on (status |= 1)
+ * $model->status->on = false; // bit 1 is now off (status &= ~1)
  */
-trait Bitflag
+class Bitflag implements JsonSerializable
 {
-    use Callback;
+    private $source;
+    private $map;
 
-    /**
-     * Define a bitflag.
-     *
-     * @param string $name The name of the flag/property.
-     * @param integer $bit The bit to flip.
-     * @param string $source The property name of the source byte on the model. 
-     */
-    public function addBitflag($name, $bit, $source)
+    public function __construct($source, array $valueMap = [])
     {
-        $name = ucfirst(Helper::denormalize($name));
-        $this->callback("get$name", function () use ($bit, $source) {
-            return (bool)($this->$source & $bit);
-        });
-        $this->callback("set$name", function ($val) use ($bit, $source) {
-            if ($val) {
-                $this->$source |= $bit;
-            } else {
-                $this->$source &= ~$bit;
-            }
-        });
+        $this->source = $source;
+        $this->map = $valueMap;
+    }
+
+    public function __set($prop, $value)
+    {
+        if ($value) {
+            $this->source |= $this->map[$prop];
+        } else {
+            $this->source &= ~$this->map[$prop];
+        }
+    }
+
+    public function __get($prop)
+    {
+        return $this->source & $this->map[$prop];
+    }
+
+    public function __toString()
+    {
+        return (string)$this->source;
+    }
+
+    public function jsonSerialize()
+    {
+        $arr = [];
+        foreach ($this->map as $key => $value) {
+            $arr[$key] = (bool)($this->source & $value);
+        }
+        return $arr;
     }
 }
 
