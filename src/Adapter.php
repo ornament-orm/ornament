d4bc947fd2c45f39aa4d5f8d4e27efd70dcc960c
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-17 10:55:32 +0200

    extend contract

--- src/Adapter.php
+++ src/Adapter.php
@@ -7,6 +7,7 @@ interface Adapter
     public function setPrimaryKey($field);
     public function setIdentifier($identifier);
     public function setFields(array $fields);
+    public function setAnnotations(array $annotations);
     public function query($model, array $ps, array $opts = []);
     public function load(Container $model);
     public function create(Container $model);
