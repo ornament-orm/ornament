7882a684ad670c9c467811b5e036b0e8151e2013
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-17 12:30:33 +0200

    make this report something actually useful

--- src/Exception/PrimaryKey.php
+++ src/Exception/PrimaryKey.php
@@ -6,12 +6,12 @@ use Exception as Ex;
 
 class PrimaryKey extends Ex
 {
-    public function __construct($model, $field, $code = 0, Ex $prev = null)
+    public function __construct($identifier, $field, $code = 0, Ex $prev = null)
     {
         $message = sprintf(
-            'Missing primary key %s on model %s',
+            'Missing primary key %s on model with identifier %s',
             $field,
-            get_class($model)
+            $identifier
         );
         parent::__construct($message, $code, $prev);
     }
