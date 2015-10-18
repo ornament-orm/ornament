47285280fae84ac611c03897870dca46c07738d2
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-18 08:10:27 +0200

    don't reload the entire model, that's useless

--- src/Query.php
+++ src/Query.php
@@ -53,7 +53,7 @@ trait Query
     {
         if ($res = self::query($where, $opts, $ctor)) {
             foreach ($res as $result) {
-                $result->load();
+                $result->load(false);
                 return $result;
             }
         }
