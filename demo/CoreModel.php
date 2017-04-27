06d9b0a6884cae0304e93ae0bcdcf873ffc93544
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-27 15:43:48 +0200

    move this to something separate for testing

--- demo/CoreModel.php
+++ demo/CoreModel.php
@@ -3,12 +3,10 @@
 namespace Ornament\Demo;
 
 use Ornament\Core\Model;
-use Ornament\Core\State;
 
 class CoreModel
 {
     use Model;
-    use State;
 
     protected $id = 1;
     public $name = 'Marijn';
