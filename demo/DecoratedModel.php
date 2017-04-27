a7f2a4a2acb60d361ca9c46597bb48b6e55a6edb
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-27 12:48:22 +0200

    move to separate files for cleanliness

--- demo/DecoratedModel.php
+++ demo/DecoratedModel.php
@@ -3,45 +3,6 @@
 namespace Ornament\Demo;
 
 use Ornament\Core\Model;
-use Ornament\Core\Decorator;
-
-class SubtractOne implements Decorator
-{
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
-    public function getSource() : int
-    {
-        return $this->value - 1;
-    }
-
-    public function __toString() : string
-    {
-        return (string)$this->getSource();
-    }
-}
-
-class DecoratorWithConstructorArguments implements Decorator
-{
-    public function __construct($value, int $one, int $two)
-    {
-        $this->value = $value;
-        $this->one = $one;
-        $this->two = $two;
-    }
-
-    public function getSource() : int
-    {
-        return $this->value * $this->one * $this->two;
-    }
-
-    public function __toString() : string
-    {
-        return (string)$this->getSource();
-    }
-}
 
 class DecoratedModel
 {
