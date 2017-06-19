6c0d85fe83c265eb45166fc116c4915325ed1653
Marijn Ophorst <marijn@sensimedia.nl>
2017-06-19 11:07:31 +0200

    this call can go

--- src/State.php
+++ src/State.php
@@ -12,11 +12,9 @@ trait State
      * Returns true if any of the model's properties was modified.
      *
      * @return bool
-     * @throws DomainException if the current object is not an Ornament model.
      */
     public function isDirty() : bool
     {
-        $this->__ornamentCheck();
         foreach ($this->__state as $prop => $val) {
             if ($this->isModified($prop)) {
                 return true;
@@ -29,11 +27,9 @@ trait State
      * Returns true if the model is still in pristine state.
      *
      * @return bool
-     * @throws DomainException if the current object is not an Ornament model.
      */
     public function isPristine() : bool
     {
-        $this->__ornamentCheck();
         return !$this->isDirty();
     }
 
@@ -41,11 +37,9 @@ trait State
      * Returns true if a specific property on the model is dirty.
      *
      * @return bool
-     * @throws DomainException if the current object is not an Ornament model.
      */
     public function isModified($property)
     {
-        $this->__ornamentCheck();
         if (!isset($this->__state->$property) && isset($this->__initial->$property)) {
             return true;
         }
@@ -62,11 +56,9 @@ trait State
      * Mark the current model as 'pristine', i.e. not dirty.
      *
      * @return void
-     * @throws DomainException if the current object is not an Ornament model.
      */
     public function markPristine()
     {
-        $this->__ornamentCheck();
         $this->__init = clone $this->__state;
     }
 }
