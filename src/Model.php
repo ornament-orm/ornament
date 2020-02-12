d8b4142d357b7059283d446d49a1d18a889617fa
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-12 06:57:58 +0100

    rewrite for php7.4; interface/usage will change slightly!

--- src/Model.php
+++ src/Model.php
@@ -5,9 +5,11 @@ namespace Ornament\Core;
 use zpt\anno\Annotations;
 use ReflectionClass;
 use ReflectionProperty;
+use ReflectionException;
 use SplObjectStorage;
 use StdClass;
 use Error;
+use Traversable;
 
 /**
  * `use` this trait to turn any vanilla class into an Ornament model.
@@ -22,69 +24,140 @@ trait Model
     private $__initial;
 
     /**
-     * @var StdClass
-     *
-     * Private storage of the model's current state.
-     */
-    private $__state;
-
-    /**
      * Constructor.
      *
-     * @param array|Traversable|null $input
+     * @param iterable|null $input
      * @return void
      */
-    public function __construct($input = null)
+    public function __construct(iterable $input = null)
     {
-        if (isset($input) && is_iterable($input)) {
+        if (isset($input)) {
             foreach ($input as $key => $value) {
-                $this->$key = $value;
+                $this->$key = $this->ornamentalize($this, $key, $value);
             }
         }
-        $this->__ornamentalize();
+        $this->__initial = clone $this;
     }
 
     /**
-     * Setup the model for decoration using Ornament. Typically called
-     * automatically on construction, but for manual implementations (e.g., a
-     * non-PDO environment) call manually when appropriate.
+     * Generate an instance from an iterable. This is similar to simply
+     * constructing an instance, but is handy to use in callbacks etc.
      *
-     * @return array Hash of Ornament annotations for this class.
+     * @param iterable $data
+     * @return object Instance of whatever class uses this trait
      */
-    private function __ornamentalize() : array
+    public static function fromIterable(iterable $data) : object
+    {
+        $class = new ReflectionClass(__CLASS__);
+        self::initTransformer();
+        return call_user_func(self::$arrayToModelTransformer, $class, $data);
+    }
+
+    /**
+     * Like `fromIterable`, only this accepts a _collection_ of iterable
+     * inputs. Use with e.g. `PDO::fetchAll()`.
+     *
+     * @param array $data
+     * @return array
+     */
+    public static function fromIterableCollection(array $collection) : array
+    {
+        array_walk($collection, function (&$item) : void {
+            $item = self::fromIterable($item);
+        });
+        return $collection;
+    }
+
+    /**
+     * Initialize the iterable-to-model transformer for this class. The default
+     * is to pass a hash of key/value pairs to the constructor.
+     *
+     * @param callable|null $transformer
+     * @return void
+     */
+    public static function initTransformer(callable $transformer = null) : void
+    {
+        if (!isset(self::$arrayToModelTransformer)) {
+            self::$arrayToModelTransformer = function (string $class, array $data) : object {
+                return new $class($data);
+            };
+        }
+        if (isset($transformer)) {
+            self::$arrayToModelTransformer = $transformer;
+        }
+    }
+
+    /**
+     * As of PHP7.4, `__set` works differently and complains about invalid types
+     * _before_ the magic method is executed.
+     *
+     * @param string $field
+     * @param mixed $value
+     * @return void
+     */
+    public function set(string $field, $value) : void
+    {
+        $this->$field = self::ornamentalize(new ReflectionClass($this), $field, $value);
+    }
+
+    /**
+     * Ornamentalize the requested field. Usually this is called for you, but on
+     * occasions you may need to call it manually.
+     *
+     * @param ReflectionClass $reflection
+     * @param string $field
+     * @param mixed $value
+     * @return mixed
+     */
+    protected static function ornamentalize(ReflectionClass $reflection, string $field, $value)
     {
         static $cache = [];
-        $class = get_called_class();
-        if (!isset($cache[$class])) {
-            $cache[$class] = [];
-            $annotator = get_class($this);
-            $reflector = new ReflectionClass($annotator);
-            $cache[$class]['class'] = new Annotations($reflector);
-            $cache[$class]['methods'] = [];
-            foreach ($reflector->getMethods() as $method) {
+        if (!$cache) {
+            $cache['class'] = new Annotations($reflection);
+            $cache['methods'] = [];
+            foreach ($reflection->getMethods() as $method) {
                 $anns = new Annotations($method);
                 $name = $method->getName();
-                $cache[$class]['methods'][$name] = $anns;
+                $cache['methods'][$name] = $anns;
             }
-            $properties = $reflector->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED & ~ReflectionProperty::IS_STATIC);
-            $cache[$class]['properties'] = [];
+            $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED & ~ReflectionProperty::IS_STATIC);
+            $cache['properties'] = [];
             foreach ($properties as $property) {
                 $name = $property->getName();
                 $anns = new Annotations($property);
+                if ((float)phpversion() >= 7.4) {
+                    if ($type = $property->getType()) {
+                        $anns['var'] = $type->getName();
+                    }
+                }
                 $anns['readOnly'] = $property->isProtected();
-                $cache[$class]['properties'][$name] = $anns;
+                $cache['properties'][$name] = $anns;
             }
         }
-        if (!isset($this->__state, $this->__initial)) {
-            $this->__state = new StdClass;
-            $this->__initial = new StdClass;
-            foreach ($cache[$class]['properties'] as $name => $anns) {
-                $this->__initial->$name = $this->$name;
-                $this->__state->$name = $this->$name;
-                unset($this->$name);
+        if (self::checkBaseType($cache['properties'][$field]['var'])) {
+            // As of PHP 7.4, type coercion is implicit when properties have
+            // been correctly type hinted.
+            if ((float)phpversion() < 7.4) {
+                settype($value, $cache['properties'][$field]['var']);
             }
+            return $value;
+        } elseif (isset($cache['properties'][$field]['var'])) {
+            if (!class_exists($cache['properties'][$field]['var'])) {
+                throw new DecoratorClassNotFoundException($cache['properties'][$field]['var']);
+            }
+            if (!array_key_exists('Ornament\Core\DecoratorInterface', class_implements($cache['properties'][$field]['var']))) {
+                throw new DecoratorClassMustImplementDecoratorInterfaceException($cache['properties'][$field]['var']);
+            }
+            $args = [];
+            if (isset($cache['properties'][$field]['construct'])) {
+                $args = is_array($cache['properties'][$field]['construct'])
+                    && isset($cache['properties'][$field]['construct'][0])
+                    && count($cache['properties'][$field]['construct']) > 1
+                    ? $cache['properties'][$field]['construct']
+                    : [$cache['properties'][$field]['construct']];
+            }
+            return new $cache['properties'][$field]['var']($value, ...$args);
         }
-        return $cache[$class];
     }
 
     /**
@@ -98,161 +171,32 @@ trait Model
      * returned instead.
      *
      * @param string $prop Name of the property.
-     * @return mixed The property's (optionally computed) value.
+     * @return mixed The property's value.
      * @throws Error if the property is unknown.
-     * @throws Ornament\Core\DecoratorClassNotFoundException if the requested
-     *  decorator class does not exists.
-     * @throws Ornament\Core\DecoratorClassMustImplementDecoratorInterfaceException
-     *  if the request decorator class does not implement the required
-     *  interface.
      */
     public function __get(string $prop)
     {
-        $annotations = $this->__ornamentalize();
-        foreach ($annotations['methods'] as $name => $anns) {
-            if (isset($anns['get']) && $anns['get'] == $prop) {
-                return call_user_func([$this, $name]);
-            }
+        try {
+            $reflection = new ReflectionProperty($this, $prop);
+        } catch (ReflectionException $e) {
+            throw new Error("Tried to get private or non-existing property $prop on ".get_class($this));
         }
-        if (!property_exists($this->__state ?? new StdClass, $prop)) {
-            $debug = debug_backtrace()[0];
-            throw new Error(
-                sprintf(
-                    "Cannot access private property %s::%s in %s:%d",
-                    get_class($this),
-                    $prop,
-                    $debug['file'] ?? 'unknown',
-                    $debug['line'] ?? 'unknown'
-                ),
-                0
-            );
+        if ($reflection->isPublic() || $reflection->isProtected()) {
+            return $this->$prop;
         }
-        if ($this->checkBaseType($annotations['properties'][$prop])) {
-            if (!is_null($this->__state->$prop)) {
-                if (is_object($this->__state->$prop) && method_exists($this->__state->$prop, '__toString')) {
-                    $this->__state->$prop = "{$this->__state->$prop}";
-                }
-                settype($this->__state->$prop, $annotations['properties'][$prop]['var']);
-            }
-        } elseif (isset($annotations['properties'][$prop]['var'])) {
-            if (!class_exists($annotations['properties'][$prop]['var'])) {
-                throw new DecoratorClassNotFoundException($annotations['properties'][$prop]['var']);
-            }
-            if (!array_key_exists('Ornament\Core\DecoratorInterface', class_implements($annotations['properties'][$prop]['var']))) {
-                throw new DecoratorClassMustImplementDecoratorInterfaceException($annotations['properties'][$prop]['var']);
-            }
-            if (!($this->__state->$prop instanceof $annotations['properties'][$prop]['var'])) {
-                $class = $annotations['properties'][$prop]['var'];
-                $args = [];
-                if (isset($annotations['properties'][$prop]['construct'])) {
-                    $args = is_array($annotations['properties'][$prop]['construct'])
-                        && isset($annotations['properties'][$prop]['construct'][0])
-                        && count($annotations['properties'][$prop]['construct']) > 1
-                        ? $annotations['properties'][$prop]['construct']
-                        : [$annotations['properties'][$prop]['construct']];
-                }
-                $this->__state->$prop = new $class($this->__state, $prop, ...$args);
-            }
-        }
-        return $this->__state->$prop;
     }
 
     /**
-     * Overloaded setter. Will only work for public properties (since protected
-     * properties are read-only).
+     * Overloaded setter.
      *
-     * If the value supplied is an instance of the `@var` annotation _and_ this
-     * is an instance of `Ornament\Core\DecoratorInterface`, it is first
-     * converted to the result of `getSource()`. Next, if a method was specified
-     * as `@set` for this property, it is called with the supplied value as a
-     * single argument and its return value is used.
-     *
-     * Otherwise, the corresponding property in `__state` is simply mutated. If
-     * an `@var` annotation is given with a scalar type, the type is coerced.
-     *
-     * @param string $prop The property to set.
-     * @param mixed $value The new value.
+     * @param string $prop
+     * @param mixed $value
      * @return mixed
-     * @throws Error if the property is private, unknown or read-only.
-    */
+     */
     public function __set(string $prop, $value)
     {
-        $annotations = $this->__ornamentalize();
-        foreach ($annotations['methods'] as $name => $anns) {
-            if (isset($anns['set']) && $anns['set'] == $prop) {
-                return call_user_func([$this, $name], $value);
-            }
-        }
-        if (isset($annotations['properties'][$prop])) {
-            if ($annotations['properties'][$prop]['readOnly']) {
-                // Modifying a readOnly (protected) property is only valid from
-                // within the same class context. This is a hacky way to check
-                // that, since `__set` obviously by default means "inside class
-                // context".
-                $debugs = debug_backtrace();
-                do {
-                    $debug = array_shift($debugs);
-                } while ($debug['function'] == '__set' && $debugs);
-
-                $error = function () use ($debugs, $prop) {
-                    $debug = $debugs[1] ?? $debugs[0];
-                    throw new Error(
-                        sprintf(
-                            "Cannot access protected property %s::%s in %s:%d",
-                            get_class($this),
-                            $prop,
-                            $debug['file'],
-                            $debug['line']
-                        ),
-                        0
-                    );
-                };
-
-                // No class context? That's definitely illegal.
-                if (!isset($debug['class'])) {
-                    $error();
-                }
-
-                // Is the calling class context not either ourselves, or a subclass?
-                // That's also illegal.
-                $reflection = new ReflectionClass($debug['class']);
-                $myclass = get_class($this);
-                if (!($reflection->getName() == $myclass || $reflection->isSubclassOf($myclass))) {
-                    $error();
-                }
-            }
-            if (isset($annotations['properties'][$prop]['var'])
-                && class_exists($annotations['properties'][$prop]['var'])
-                && array_key_exists(
-                    'Ornament\Core\DecoratorInterface',
-                    class_implements($annotations['properties'][$prop]['var'])
-                )
-                && $value instanceof $annotations['properties'][$prop]['var']
-            ) {
-                $value = $value->getSource();
-            }
-            if ($this->checkBaseType($annotations['properties'][$prop]) && !is_null($value)) {
-                if (is_object($value) && method_exists($value, '__toString')) {
-                    $value = "$value";
-                }
-                settype($value, $annotations['properties'][$prop]['var']);
-            }
-        }
-        if (!property_exists($this->__state ?? new StdClass, $prop)) {
-            $debug = debug_backtrace()[0];
-            throw new Error(
-                sprintf(
-                    "Cannot access private or unknown property %s::%s in %s:%d",
-                    get_class($this),
-                    $prop,
-                    $debug['file'] ?? 'unknown',
-                    $debug['line'] ?? 'unknown'
-                ),
-                0
-            );
-        }
-        $this->__state->$prop = $value;
-        return $value;
+        $this->set($prop, $value);
+        return $this->$prop;
     }
     
     /**
@@ -264,14 +208,15 @@ trait Model
      */
     public function __isset(string $prop) : bool
     {
-        $annotations = $this->__ornamentalize();
-        foreach ($annotations['methods'] as $name => $anns) {
-            if (isset($anns['get']) && $anns['get'] == $prop) {
-                return true;
-            }
+        try {
+            $reflection = new ReflectionProperty($this, $prop);
+        } catch (ReflectionException $e) {
+            return false;
         }
-        return property_exists($this->__state, $prop)
-            && !is_null($this->__state->$prop);
+        if ($reflection->isPublic() || $reflection->isProtected()) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -290,13 +235,13 @@ trait Model
      * Internal helper method to check if the given property is annotated as one
      * of PHP's internal base types (int, float etc).
      *
-     * @param zpt\anno\Annotations $prop
+     * @param string|null $type
      * @return bool
      */
-    protected function checkBaseType(Annotations $prop) : bool
+    protected static function checkBaseType(string $type = null) : bool
     {
         static $baseTypes = ['bool', 'int', 'float', 'string', 'array', 'object', 'null'];
-        return in_array($prop['var'] ?? null, $baseTypes);
+        return in_array($type, $baseTypes);
     }
 }
 
