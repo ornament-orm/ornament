c1118e565022e8c7c6c18b3fd470e986d6309f8e
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-12 06:57:13 +0100

    interface has changed slightly

--- demo/DecoratorWithConstructorArguments.php
+++ demo/DecoratorWithConstructorArguments.php
@@ -7,9 +7,9 @@ use StdClass;
 
 class DecoratorWithConstructorArguments extends Decorator
 {
-    public function __construct(StdClass $model, string $property, int $one, int $two)
+    public function __construct($value, int $one, int $two)
     {
-        parent::__construct($model, $property);
+        parent::__construct($value);
         $this->one = $one;
         $this->two = $two;
     }
