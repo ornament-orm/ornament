1df73311c9a283ff8bf436ebb8ff8b3a0dc4a142
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-12 08:33:00 +0100

    special version for php7.4 so we can test both

---
+++ demo/CoreModel74.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ornament\Demo;
+
+use Ornament\Core\Model;
+
+class CoreModel74
+{
+    use Model;
+
+    protected int $id = 1;
+
+    public string $name = 'Marijn';
+
+    private bool $invisible = true;
+}
+
