7dee313c3852473a93e22a7d7765e253c2990407
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-18 06:59:44 +0100

    add decorators for a getter method

--- demo/DecoratedModel74.php
+++ demo/DecoratedModel74.php
@@ -20,5 +20,14 @@ class DecoratedModel74
      * @construct 3
      */
     public DecoratorWithConstructorArguments $anotherField;
+
+    /**
+     * @get virtual_property
+     * @return string
+     */
+    protected function getVirtualPropertyDemo() : string
+    {
+        return "{$this->field} {$this->anotherField}";
+    }
 }
 
